02318a6495e6d7e4052654b1db8ea513531fc2d6
Kristina <40335@mail.ru>
2022-10-17 11:44:56 +0300

    add Task_1

--- Task_1.py
+++ Task_1.py
@@ -8,3 +8,37 @@
 # Пример:
 # 1+2*3 => 7;
 # (1+2)*3 => 9;
+
+# expression = input('Введите арифметическое выражение:  ' )
+# print(eval(expression))
+
+import re
+ 
+ 
+actions = {
+  "^": lambda x, y: str(float(x)**float(y)),
+  "*": lambda x, y: str(float(x) * float(y)),
+  "/": lambda x, y: str(float(x) / float(y)),
+  "+": lambda x, y: str(float(x) + float(y)),
+  "-": lambda x, y: str(float(x) - float(y))
+}
+ 
+priority_reg_exp = r"\((.+?)\)"
+action_reg_exp = r"(-?\d+(?:\.\d+)?)\s*\{}\s*(-?\d+(?:\.\d+)?)"
+ 
+ 
+ 
+def my_eval(expresion: str) -> str:
+ 
+    while (match := re.search(priority_reg_exp, expresion)):
+        expresion: str = expresion.replace(match.group(0), my_eval(match.group(1)))
+ 
+    for symbol, action in actions.items():
+        while (match := re.search(action_reg_exp.format(symbol), expresion)):
+            expresion: str = expresion.replace(match.group(0), action(*match.groups()))
+ 
+    return expresion
+ 
+ 
+exp = "(1 + 4) * (5 * (10 - 2)) / 5"
+print(my_eval(exp))
